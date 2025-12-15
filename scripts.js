5f758a3f9b455705333e33abbdfb7f4b4568a578
nayryroc <78516345+nayryroc@users.noreply.github.com>
2025-12-15 12:54:19 -0500

    added sections
    added new sections to page
    added english and italian page

--- scripts.js
+++ scripts.js
@@ -1,1 +1,17 @@
-console.log("test");
+window.addEventListener('resize', function(event) {
+    visionTile();
+}, true);
+
+window.addEventListener('load', function(event){
+    visionTile();
+}, true);
+
+function visionTile(){
+    
+
+    let tiles = document.getElementsByClassName("vision2__content");
+    for(let i = 0; i < tiles.length; i++){
+        tiles[i].style.transform = "translateY(" + tiles[i].children[1].offsetHeight + "px)";
+        console.log("run");
+    }
+}
